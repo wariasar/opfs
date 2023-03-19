965be7df9532ae9fe2af9be5942a76b7f5c62e48
Armin <jaap@warias.org>
2023-03-19 23:08:43 +0100

    style iprovements, status message

--- opfs.js
+++ opfs.js
@@ -13,8 +13,10 @@ loadButton.addEventListener("click", (event) => {
 
 worker.onmessage = function (message) {
    if(message.data[0] == "w")	
-      alert (message.data[1]);
-   if (message.data[0] == "r")
+      document.getElementById("fstatus").innerHTML = "File written";
+   if (message.data[0] == "r") {
+      document.getElementById("fstatus").innerHTML = "File reloaded";
       document.getElementById("text").value = message.data[1];
+   }
 }
 
